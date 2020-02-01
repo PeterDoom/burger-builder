a8c09ffb4f774b52084f4ff7e661e5fa8c93645b
PeterDoom <nanev0205@gmail.com>
2020-02-01 18:44:30 +0200

    Adding NavBar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/AuxComponent";
 import classes from "./Layout.module.css"
+import Toolbar from "../Navigation/Toolbar/Toolbar"
 
 const layout = props => (
     <Aux>
-        <div>Tab, SideBar, Drawer</div>
+        <Toolbar/>
         <main className={classes.Content}>
             {props.children}
         </main>
