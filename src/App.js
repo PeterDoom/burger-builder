a887ab030ce86c5f33ded300e972bcffa109d14a
PeterDoom <nanev0205@gmail.com>
2020-03-29 21:31:26 +0300

    Adding react-router-dom + Adding Checkout component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, {Component} from 'react';
-import './App.css';
-import Layout from './components/Layout/Layout';
+import React, { Component } from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Layout>
-                  <BurgerBuilder/>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/" exact component={BurgerBuilder} />
+                    </Switch>
                 </Layout>
             </div>
         );
-    };
+    }
 }
 
 export default App;
-
 
 //https://react-burger-bd72f.firebaseio.com/
