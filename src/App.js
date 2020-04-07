cb128d1d9573ce8c4c98184107bb6d9354338b48
PeterDoom <nanev0205@gmail.com>
2020-04-08 00:44:03 +0300

    Adding proper orders menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders"
 import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
                 <Layout>
                     <Switch>
                         <Route path="/checkout" component={Checkout} />
+                        <Route path="/orders" component={Orders}/>
                         <Route path="/" exact component={BurgerBuilder} />
                     </Switch>
                 </Layout>
