248e89f02d8381dbdee718cf024887e78660d766
PeterDoom <nanev0205@gmail.com>
2020-05-01 23:42:42 +0300

    fixed typos

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -5,11 +5,11 @@ const initialState = {
     token: null,
     userId: null,
     error: null,
-    laoding: false,
+    loading: false,
 };
 
 const authStart = (state, action) => {
-    return updateObject(state, { error: null, laoding: true });
+    return updateObject(state, { error: null, loading: true });
 };
 
 const authSuccess = (state, action) => {
@@ -17,22 +17,22 @@ const authSuccess = (state, action) => {
         token: action.idToken,
         userId: action.userId,
         error: null,
-        laoding: false
+        loading: false
     })
-}
+};
 
 const authFail = (state, action) => {
     return updateObject(state, {
         error: action.error,
-        laoding: false
+        loading: false
     })
-}
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.AUTH_START: return authStart(state, action)
-        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action)
-        case actionTypes.AUTH_FAIL: return authFail(state, action)
+        case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
         default:
             return state;
     }
