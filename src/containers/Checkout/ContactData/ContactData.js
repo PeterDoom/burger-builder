7dace7f6c8c62b44c7e482d97bb879b6163b0b6f
PeterDoom <nanev0205@gmail.com>
2020-05-17 20:47:02 +0300

    Refactoring ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from "../../../components/UI/Input/Input";
 import {connect} from "react-redux";
 import withErrorHandler from "../../../hoc/withErrorHandling/withErrorHandler"
 import * as orderType from "../../../store/actions/index"
+import {updateObject} from "../../../shared/utility"
 
 class ContactData extends Component {
     state = {
@@ -111,16 +112,20 @@ class ContactData extends Component {
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm,
-        };
-        const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(
-            updatedFormElement.value,
-            updatedFormElement.validation
-        );
-        updatedFormElement.touched = true;
+
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            valid:this.checkValidity(
+                event.target.value,
+                event.target.validation
+            ),
+            touched: true
+        }) ;
+
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier] : updatedFormElement
+        })
+
         let formIsValid = true;
 
         for (let inputId in updatedOrderForm) {
